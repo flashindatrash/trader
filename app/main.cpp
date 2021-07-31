105512e9a6c44321da4f864af2591b5ebdec9c7b
v.proskurin <v.proskurin@studionx.ru>
2021-07-31 20:37:45 +0300

    add libraries CMakeLists

---
+++ app/main.cpp
@@ -0,0 +1,12 @@
+#include <argparser/ArgumentParser.hpp>
+#include "TraderApp.hpp"
+
+int main(int argc, char** argv) {
+	srand(time(NULL));
+
+	std::string cfg_file;
+	cppargparser::ArgumentParser args;
+
+    TraderApp::create();
+	return EXIT_SUCCESS;
+}
