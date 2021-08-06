785f751537fae8a847c4e2cde705bb79d29703b6
Vadim Proskurin <uareweirdo@gmail.com>
2021-08-07 02:20:51 +0300

    ignore opening eqal transactions

--- trader/algorithm/TraderManager.cpp
+++ trader/algorithm/TraderManager.cpp
@@ -10,6 +10,7 @@
 
 static float sMinRate = 0.003f;
 static float sMaxRate = 0.01f;
+static float sEqualRate = 0.02f;
 
 TraderManager::TraderManager(OrderManager& orders)
     : BaseManager(orders)
@@ -41,10 +42,27 @@ bool TraderManager::check(const TradeSymbol& symbol) {
 
     float change = util::get_percent(kline.priceOpen, kline.priceClose);
     float expected = sMinRate + (change > 0 ? baseK : quoteK) * (sMaxRate - sMinRate);
-    trace("change: %f, expected: %f\n", change, expected);
     if (std::abs(change) < expected)
         return false;
 
     std::string side = change > 0.0f ? "SELL" : "BUY";
+
+    // не дублируем схожие транзакции
+    if (hasEqualTransaction(side, symbol.getPrice()))
+        return false;
+
     return _orders.create(symbol, side, _min_quantity, true);
+}
+
+bool TraderManager::hasEqualTransaction(const std::string& side, double price) const {
+    for (const BinanceOrderData& order : _orders.getTransactions()) {
+        // интересуют ордеры с одним типом
+        if (order.side != side)
+            continue;
+        // проверяем разницу в цене на минимальный порог
+        double change = util::get_percent(order.getPrice(), price);
+        if (std::abs(change) < sEqualRate)
+            return true;
+    }
+    return false;
 }
--- trader/algorithm/TraderManager.hpp
+++ trader/algorithm/TraderManager.hpp
@@ -9,6 +9,10 @@ public: // methods
 
     bool check(const TradeSymbol& symbol) override;
 
+protected: // methods
+    // найти схожую транзакцию по цене
+    bool hasEqualTransaction(const std::string& side, double price) const;
+
 protected: // vars
     double _min_quantity = 0.0;
 };
