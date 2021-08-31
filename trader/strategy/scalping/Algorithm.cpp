23ecbbc0c327aa6c81c8d423f42a7a223c28e4d0
v.proskurin <v.proskurin@studionx.ru>
2021-09-01 01:24:37 +0300

    emergency trade

--- trader/strategy/scalping/Algorithm.cpp
+++ trader/strategy/scalping/Algorithm.cpp
@@ -129,10 +129,18 @@ bool Algorithm::tryOpenPosition(const Context& context) {
         if (_settings.open_max_multiply > 1.0)
             request.quantity = std::min(request.quantity, Exchanger().minQuantity(request.symbol) * _settings.open_max_multiply);
     } else {
-        // ближашая позиция уже имеет профит, или дистанция меньше допустимого шага
-        // дождемся получения прибыли с нее, или изменению в проигрышную сторону
-        Logger::trace("open: waiting profit %f >= %f", last->distance(context.price()), -last->price() * _settings.open_price_percent);
-        return false;
+        request.quantity = last->baseQuantity();
+        if (not request.isEnough()) {
+            // произошла беда, мы потратили все деньги, и не можем закрыть сделку
+            // в рамках экстренной ситуации выполняем сделку с минимальным лотом
+            Logger::trace("open: emergency");
+            request.quantity = Exchanger().minQuantity(request.symbol);
+        } else {
+            // ближашая позиция уже имеет профит, или дистанция меньше допустимого шага
+            // дождемся получения прибыли с нее, или изменению в проигрышную сторону
+            Logger::trace("open: waiting profit");
+            return false;
+        }
     }
 
     // посчитаем расход данной сделки
