ce8fa11f3a7480d7113d97d4711e2f467528c94a
Vadim <uareweirdo@gmail.com>
2021-09-02 21:11:02 +0300

    fix percent

--- trader/strategy/scalping/Status.cpp
+++ trader/strategy/scalping/Status.cpp
@@ -43,8 +43,8 @@ void Status::printTimeline(Positions &positions, Price current, const Settings& 
 
     const auto profitable = positions.compare_if(Predicates::closable(settings.symbol), Compares::distance(current));
     if (profitable != positions.cend()) {
-        double percent = profitable->distance(current) / (current * settings.close_position_percent * 100.0);
-        timeline.append(" [" + std::to_string((int)percent) + "%]");
+        double percent = profitable->distance(current) / (current * settings.close_position_percent);
+        timeline.append(" [" + std::to_string((int)(percent * 100.0)) + "%]");
     }
 
     Logger::status(timeline.c_str());
