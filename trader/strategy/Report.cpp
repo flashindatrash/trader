a42d73580f4647f87ab347cdb9dd99cccc4fb904
v.proskurin <v.proskurin@studionx.ru>
2021-09-30 02:11:02 +0300

    fix build

--- trader/strategy/Report.cpp
+++ trader/strategy/Report.cpp
@@ -10,8 +10,6 @@ NS_USE
 Quantity Report::add(const Position& open, const Position& close) {
     Quantity added = open.profit(close.price()) - close.fee();
     profit += added;
-    add_base += close.baseQuantity() - open.baseQuantity();
-    add_quote += close.quoteQuantity() - open.quoteQuantity();
     use_base = std::max(use_base, close.baseQuantity());
     use_quote = std::max(use_quote, close.quoteQuantity());
     return added;
