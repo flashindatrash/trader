11ee1425f8c81b55da59689b7fc8e690447bea59
v.proskurin <v.proskurin@studionx.ru>
2021-10-29 00:38:54 +0300

    create MACD indicator

---
+++ trader/exchanger/indicator/MACD.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Вадим Проскурин on 29.10.2021.
+//
+
+#include "MACD.hpp"
---
+++ trader/exchanger/indicator/MACD.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Вадим Проскурин on 29.10.2021.
+//
+
+
+#pragma once
+
+
+
+class MACD {
+
+};
+
+
