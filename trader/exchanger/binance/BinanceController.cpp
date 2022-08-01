6ea3427e52e7dd1d8e5717d5e6439c43d92134aa
flashindatrash <uareweirdo@gmail.com>
2022-08-01 10:10:33 +0300

    revert cent value for staking

--- trader/exchanger/binance/BinanceController.cpp
+++ trader/exchanger/binance/BinanceController.cpp
@@ -595,7 +595,7 @@ bool BinanceController::stake(StakingWrapper& container, StakingRequest& request
         return false;
 
     // round amount
-    static const double cent = 0.00000001;
+    double cent = container.minimum() / 10.0;
     request.amount = std::floor(request.amount / cent) * cent;
 
     // check is enough to stake
