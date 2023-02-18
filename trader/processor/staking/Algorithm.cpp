b31e7df875f0ac7273e44034bd1c23295526fce9
flashindatrash <uareweirdo@gmail.com>
2023-02-18 21:57:58 +0200

    add band asset to check staking

--- trader/processor/staking/Algorithm.cpp
+++ trader/processor/staking/Algorithm.cpp
@@ -52,7 +52,7 @@ bool Algorithm::init() {
         if (staking->apy() < 0.06)
             continue;
 
-        static const std::set<std::string> active = {"SOL", "CELR", "COS", "FTM", "SKL", "CHR", "ICX", "CTK", "APE", "XTZ", "KAVA", "GLMR", "TOMO", "FRONT", "AVAX", "LEVER", "THETA", "SAND", "STMX", "LIT", "DOT", "VET", "REEF", "CAKE", "MINA", "BTTC", "ATOM", "EOS", "IOST", "ACH", "DEXE", "STX", "CHESS", "REI", "ADA", "NEO", "BSW", "DODO", "ROSE", "MC", "ALGO", "MBOX", "NEAR", "ONT", "ONE", "WAVES", "OM", "ICP", "FIO", "TKO", "ZIL", "EGLD", "OGN", "TRU", "KSM", "AXS", "FUN", "USTC"};
+        static const std::set<std::string> active = {"BAND", "SOL", "CELR", "COS", "FTM", "SKL", "CHR", "ICX", "CTK", "APE", "XTZ", "KAVA", "GLMR", "TOMO", "FRONT", "AVAX", "LEVER", "THETA", "SAND", "STMX", "LIT", "DOT", "VET", "REEF", "CAKE", "MINA", "BTTC", "ATOM", "EOS", "IOST", "ACH", "DEXE", "STX", "CHESS", "REI", "ADA", "NEO", "BSW", "DODO", "ROSE", "MC", "ALGO", "MBOX", "NEAR", "ONT", "ONE", "WAVES", "OM", "ICP", "FIO", "TKO", "ZIL", "EGLD", "OGN", "TRU", "KSM", "AXS", "FUN", "USTC"};
         if (active.count(staking->asset()))
             continue;
 
